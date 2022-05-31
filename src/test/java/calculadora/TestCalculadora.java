4960620ea993a237d534cc29c86d4f32623a1d24
Vinícius <vinicius--cruz@hotmail.com>
2022-05-31 20:53:00 -0300

    Update

--- src/test/java/calculadora/TestCalculadora.java
+++ src/test/java/calculadora/TestCalculadora.java
@@ -20,4 +20,12 @@ public class TestCalculadora {
         assertEquals(retornoEsperado, retornoFeito, 0);
     }
 
+    @Test
+    public void testGetProduto() {
+        Calculadora calculadora = new Calculadora(4.0, 2.0);
+        double retornoEsperado = 8.0;
+        double retornoFeito = calculadora.getProduto();
+        assertEquals(retornoEsperado, retornoFeito, 0);
+    }
+
 }
